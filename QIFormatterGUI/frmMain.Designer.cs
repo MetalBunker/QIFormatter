f092eb93cad484619fc2fdf64e6618db25b5db9f
LucasMetal <lucasmetal@hotmail.com>
2015-06-06 17:58:23 -0300

    - First commit!

---
+++ QIFormatterGUI/frmMain.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace QIFormatterGUI
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.txtOutputFilePath = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnGenerateQIF = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnClear = new System.Windows.Forms.ToolStripButton();
+            this.btnPaste = new System.Windows.Forms.ToolStripButton();
+            this.lblTransactionsCount = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.sfdQIF = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtData
+            // 
+            this.txtData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtData.Location = new System.Drawing.Point(2, 82);
+            this.txtData.Margin = new System.Windows.Forms.Padding(2);
+            this.txtData.Multiline = true;
+            this.txtData.Name = "txtData";
+            this.txtData.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtData.Size = new System.Drawing.Size(673, 414);
+            this.txtData.TabIndex = 0;
+            this.txtData.TextChanged += new System.EventHandler(this.txtData_TextChanged);
+            // 
+            // txtOutputFilePath
+            // 
+            this.txtOutputFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutputFilePath.Location = new System.Drawing.Point(46, 24);
+            this.txtOutputFilePath.Margin = new System.Windows.Forms.Padding(2);
+            this.txtOutputFilePath.Name = "txtOutputFilePath";
+            this.txtOutputFilePath.Size = new System.Drawing.Size(494, 20);
+            this.txtOutputFilePath.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.txtData, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.toolStrip1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(677, 498);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSelectFile);
+            this.groupBox1.Controls.Add(this.btnGenerateQIF);
+            this.groupBox1.Controls.Add(this.txtOutputFilePath);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(671, 54);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(9, 22);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(32, 23);
+            this.btnSelectFile.TabIndex = 4;
+            this.btnSelectFile.Text = "File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // btnGenerateQIF
+            // 
+            this.btnGenerateQIF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerateQIF.Location = new System.Drawing.Point(561, 13);
+            this.btnGenerateQIF.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGenerateQIF.Name = "btnGenerateQIF";
+            this.btnGenerateQIF.Size = new System.Drawing.Size(105, 36);
+            this.btnGenerateQIF.TabIndex = 3;
+            this.btnGenerateQIF.Text = "Generate QIF";
+            this.btnGenerateQIF.UseVisualStyleBackColor = true;
+            this.btnGenerateQIF.Click += new System.EventHandler(this.btnGenerateQIF_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnClear,
+            this.btnPaste,
+            this.lblTransactionsCount,
+            this.toolStripSeparator1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 60);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(677, 20);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnClear
+            // 
+            this.btnClear.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnClear.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.Image")));
+            this.btnClear.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(38, 17);
+            this.btnClear.Text = "Clear";
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnPaste.Image = ((System.Drawing.Image)(resources.GetObject("btnPaste.Image")));
+            this.btnPaste.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(39, 17);
+            this.btnPaste.Text = "Paste";
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // lblTransactionsCount
+            // 
+            this.lblTransactionsCount.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.lblTransactionsCount.Name = "lblTransactionsCount";
+            this.lblTransactionsCount.Size = new System.Drawing.Size(80, 17);
+            this.lblTransactionsCount.Text = "0 transactions";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 20);
+            // 
+            // sfdQIF
+            // 
+            this.sfdQIF.DefaultExt = "qif";
+            this.sfdQIF.Filter = "QIF Files (*.qif)|*.qif";
+            this.sfdQIF.Title = "Select QIF filename to generate...";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 498);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmMain";
+            this.Text = "QIFormatter";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.TextBox txtOutputFilePath;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGenerateQIF;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnClear;
+        private System.Windows.Forms.ToolStripButton btnPaste;
+        private System.Windows.Forms.ToolStripLabel lblTransactionsCount;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.SaveFileDialog sfdQIF;
+    }
+}
+
---
+++ QIFormatterGUI/frmMain.cs
@@ -0,0 +1,101 @@
+﻿using QifApi;
+using QifApi.Transactions;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QIFormatterGUI
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerateQIF_Click(object sender, EventArgs e)
+        {
+            if (txtOutputFilePath.TextLength == 0)
+            {
+                MessageBox.Show("A dónde querés que lo guarde goma?", "Zapato!");
+                return;
+            }
+
+            if (txtData.TextLength == 0)
+            {
+                MessageBox.Show("Qué datos querés que exporte goma?", "Zapato!");
+                return;
+            }
+
+            try
+            {
+                QifDom targetFile = new QifDom();
+                BasicTransaction trans = null;
+                string[] recordValues = null;
+
+                foreach (var record in txtData.Lines)
+                {
+                    recordValues = record.Split('\t');
+
+                    trans = new BasicTransaction();
+
+                    trans.Date      = DateTime.ParseExact(recordValues[0], "yyyy-MM-dd", null);
+                    trans.Payee     = recordValues[1];
+                    trans.Memo      = recordValues[2];
+                    trans.Amount    = decimal.Parse(recordValues[3], CultureInfo.InvariantCulture) * -1; //Multiplico por -1 para que quede como un gasto desde la cuenta
+
+                    targetFile.CashTransactions.Add(trans);
+                }
+
+                targetFile.Export(txtOutputFilePath.Text, Encoding.Default);
+
+                MessageBox.Show("Saved! Cantidad registros guardados: " + targetFile.CashTransactions.Count);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Voló todo al carajo: " + ex.Message, "Boom!");
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtData.Clear();
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                txtData.Text+= Clipboard.GetText();
+            }
+        }
+
+        private void txtData_TextChanged(object sender, EventArgs e)
+        {
+            lblTransactionsCount.Text = txtData.Lines.Length + " transactions";
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            if (txtOutputFilePath.Text.Length > 0)
+            {
+                sfdQIF.FileName = txtOutputFilePath.Text;
+            }
+
+            if (sfdQIF.ShowDialog() == DialogResult.OK)
+            {
+                txtOutputFilePath.Text = sfdQIF.FileName;
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            txtOutputFilePath.Text = Path.Combine(  Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
+                                                    "CC_Export_" + DateTime.Now.ToString("yyyy-MM-dd_HHmm") + ".qif");
+        }
+       
+    }
+}
